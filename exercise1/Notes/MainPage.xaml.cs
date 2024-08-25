332fbc1e4b1a221adceb92838fdd831deb1f979a
qi-win <albert.tan.2.pi.0@outlook.com>
2024-08-25 20:29:22 -0300

    📦 NEW: add button to save

--- exercise1/Notes/MainPage.xaml.cs
+++ exercise1/Notes/MainPage.xaml.cs
@@ -3,35 +3,36 @@
 public partial class MainPage : ContentPage
 {
     string _fileName = Path.Combine(FileSystem.AppDataDirectory, "notes.txt");
-    Editor editor;
+    //Editor editor;
 
     public MainPage()
     {
-        var notesHeading = new Label() { Text = "Notes TEST", HorizontalOptions = LayoutOptions.Center, FontAttributes = FontAttributes.Bold };
+        InitializeComponent();
+        // var notesHeading = new Label() { Text = "Notes TEST", HorizontalOptions = LayoutOptions.Center, FontAttributes = FontAttributes.Bold };
 
-        editor = new Editor() { Placeholder = "Enter your note", HeightRequest = 100 };
-        editor.Text = File.Exists(_fileName) ? File.ReadAllText(_fileName) : string.Empty;
+        // editor = new Editor() { Placeholder = "Enter your note", HeightRequest = 100 };
+        // editor.Text = File.Exists(_fileName) ? File.ReadAllText(_fileName) : string.Empty;
 
-        var buttonsGrid = new Grid() { HeightRequest = 40.0 };
-        buttonsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1.0, GridUnitType.Auto) });
-        buttonsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(30.0, GridUnitType.Absolute) });
-        buttonsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1.0, GridUnitType.Auto) });
+        // var buttonsGrid = new Grid() { HeightRequest = 40.0 };
+        // buttonsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1.0, GridUnitType.Auto) });
+        // buttonsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(30.0, GridUnitType.Absolute) });
+        // buttonsGrid.ColumnDefinitions.Add(new ColumnDefinition() { Width = new GridLength(1.0, GridUnitType.Auto) });
 
-        var saveButton = new Button() { WidthRequest = 100, Text = "Save" };
-        saveButton.Clicked += OnSaveButtonClicked;
-        Grid.SetColumn(saveButton, 0);
-        buttonsGrid.Children.Add(saveButton);
+        // var saveButton = new Button() { WidthRequest = 100, Text = "Save" };
+        // saveButton.Clicked += OnSaveButtonClicked;
+        // Grid.SetColumn(saveButton, 0);
+        // buttonsGrid.Children.Add(saveButton);
 
-        var deleteButton = new Button() { WidthRequest = 100, Text = "Delete" };
-        deleteButton.Clicked += OnDeleteButtonClicked;
-        Grid.SetColumn(deleteButton, 2);
-        buttonsGrid.Children.Add(deleteButton);
+        // var deleteButton = new Button() { WidthRequest = 100, Text = "Delete" };
+        // deleteButton.Clicked += OnDeleteButtonClicked;
+        // Grid.SetColumn(deleteButton, 2);
+        // buttonsGrid.Children.Add(deleteButton);
 
-        var stackLayout = new VerticalStackLayout 
-        { 
-            Padding = new Thickness(30, 60, 30, 30),
-            Children = { notesHeading, editor, buttonsGrid }
-        };
+        // var stackLayout = new VerticalStackLayout 
+        // { 
+        //     Padding = new Thickness(30, 60, 30, 30),
+        //     Children = { notesHeading, editor, buttonsGrid }
+        // };
 
         //this.Content = stackLayout;
     }
